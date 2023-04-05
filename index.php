c7b14b7338a27ead3514637ff7333da0f66cc9c8
DriverSpy <59536815+driverspy@users.noreply.github.com>
2023-04-05 17:42:08 +0200

    Recode
    Recode

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 date_default_timezone_set('Europe/Berlin');
 
 if (isset($_GET['domain'])) {
-$domain1 = " bei <b>" . $_GET['domain'] . "</b>";
-$domain2 = " von <b>" . $_GET['domain'] . "</b>";
+$domain1 = " bei <span class='text-warning'>" . $_GET['domain'] . "</span>";
+$domain2 = " von <span class='text-warning'>" . $_GET['domain'] . "</span>";
 $domain3 = $_GET['domain'] . " Support-Team";
 } else {
 	$domain1 = $domain2 = $domain3 = '';
@@ -29,153 +29,134 @@ if (isset($_GET['signature'])) {
 	$signature = true;
 }
 
-if (isset($_GET['usage'])) {
-	if($_GET['usage'] == '0') {
-		$usage = false;
-	} else {
-		$usage = true;
-	}
-} else {
-	$usage = true;
-}
 ?>
 <!DOCTYPE html>
 <html lang="de">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>NichtHallo.DE</title>
-	<meta name="description" content="Sage nicht einfach nur 'Hallo' im Chat!">
-	<meta name="keywords" content="nichthallo,hallo,nohello">
-	<link rel="icon" type="image/png" href="assets/images/nichthallo.png" />
+  <meta charset="utf-8" />
+  <meta name="language" content="de">
+  <meta name="robots" content="index, follow">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Sage nicht einfach nur 'Hallo' im Chat!">
+  <meta name="keywords" content="nichthallo,hallo,nohello">
 
-	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
-
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-
-	<style>	
-		@font-face {
-			font-family: LilitaOne;
-			src: url(assets/fonts/LilitaOne-Regular.ttf) format('truetype');
-			font-weight: normal;
-    		font-style: normal;
-		}
-		@font-face {
-			font-family: SourceSansPro;
-			src: url(assets/fonts/SourceSansPro-Regular.ttf) format('truetype');
-			font-weight: normal;
-    		font-style: normal;
-		}
-		 
-		.slide-in-bottom{-webkit-animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) both;animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) both}
-		.slide-in-bottom-h1{-webkit-animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) .5s both;animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) .5s both}
-		.slide-in-bottom-subtitle{-webkit-animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) .75s both;animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) .75s both}
-		.fade-in{-webkit-animation:fade-in 1.2s cubic-bezier(.39,.575,.565,1.000) 1s both;animation:fade-in 1.2s cubic-bezier(.39,.575,.565,1.000) 1s both}
-		.bounce-top-icons{-webkit-animation:bounce-top .9s 1s both;animation:bounce-top .9s 1s both}
-
-		.font-LO {
-			font-family: LilitaOne;
-		}
-
-		.imgShadow {
-			filter: drop-shadow(3px 3px 1px #00000045);
-			margin-right: 5px;
-		}
-
-		.li-ns::marker {
-			content: '● ';
-			color: #ef4545;
-		}
-		.li-ss::marker {
-			content: '● ';
-			color: #11b981;
-		}
-		
-		@-webkit-keyframes slide-in-bottom{0%{-webkit-transform:translateY(1000px);transform:translateY(1000px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes slide-in-bottom{0%{-webkit-transform:translateY(1000px);transform:translateY(1000px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}
-		@-webkit-keyframes bounce-top{0%{-webkit-transform:translateY(-45px);transform:translateY(-45px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in;opacity:1}24%{opacity:1}40%{-webkit-transform:translateY(-24px);transform:translateY(-24px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}65%{-webkit-transform:translateY(-12px);transform:translateY(-12px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}82%{-webkit-transform:translateY(-6px);transform:translateY(-6px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}93%{-webkit-transform:translateY(-4px);transform:translateY(-4px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}25%,55%,75%,87%{-webkit-transform:translateY(0);transform:translateY(0);-webkit-animation-timing-function:ease-out;animation-timing-function:ease-out}100%{-webkit-transform:translateY(0);transform:translateY(0);-webkit-animation-timing-function:ease-out;animation-timing-function:ease-out;opacity:1}}@keyframes bounce-top{0%{-webkit-transform:translateY(-45px);transform:translateY(-45px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in;opacity:1}24%{opacity:1}40%{-webkit-transform:translateY(-24px);transform:translateY(-24px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}65%{-webkit-transform:translateY(-12px);transform:translateY(-12px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}82%{-webkit-transform:translateY(-6px);transform:translateY(-6px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}93%{-webkit-transform:translateY(-4px);transform:translateY(-4px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}25%,55%,75%,87%{-webkit-transform:translateY(0);transform:translateY(0);-webkit-animation-timing-function:ease-out;animation-timing-function:ease-out}100%{-webkit-transform:translateY(0);transform:translateY(0);-webkit-animation-timing-function:ease-out;animation-timing-function:ease-out;opacity:1}}
-		@-webkit-keyframes fade-in{0%{opacity:0}100%{opacity:1}}@keyframes fade-in{0%{opacity:0}100%{opacity:1}}
-				
-	</style>
-
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/nichthallo.png">
+  <link rel="icon" type="image/png" href="assets/img/nichthallo.png" />
+  
+  <title>
+    NichtHallo.DE
+  </title>
+  <script src="./assets/js/plugins/typedjs.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v6.3.0/css/all.css" rel="stylesheet">
+  <link href="./assets/css/blk-design-system-pro.css?v=1.0.0" rel="stylesheet" />
 </head>
 
+<body class="index-page">
 
-<body class="leading-normal tracking-normal text-gray-900" style="font-family: SourceSansPro, sans-serif;">
+  <nav class="navbar navbar-expand-lg fixed-top navbar-transparent" color-on-scroll="300">
+    <div class="container">
+      <div class="navbar-translate">
+        <p class="navbar-brand">
+          <span>Nicht[<span class="text-primary" id="typed"></span>].DE</span>
+        </p>
+      </div>
+    </div>
+  </nav>
 
+  <div class="wrapper">
+    <div class="main">
+      <div class="contactus-3">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 ml-auto mr-auto">
+              <h3 style="font-size: 2.5rem;" class="title">Sage nicht einfach nur "Hallo" im Chat!</h3>
+              <p>Stell dir vor, du bittest um Hilfe im Support<?php echo $domain1 ?> und möchtest schnell Hilfe bekommen.
+                <br>Genau das ist das Ziel der meisten Plattformen, Firmen, Anbieter, Dienstleister, usw., dir die schnellstmögliche Hilfe anzubieten.
+                <br>Nun schreibst du aber nur <i>"Hallo!"</i> um ein Supportgespräch zu beginnen.
+                <br>Das ist absolut nicht von Vorteil und erschwert das Ziel, schnelle Hilfe zu bieten, ungemein.
+                <br>Probier es stattdessen doch einfach mal mit einer direkten Beschreibung deines Anliegens.<br>
+			            Ein Support-Mitarbeiter<?php echo $domain2 ?> wird sich so-oder-so zurückmelden, um dein Anliegen anzugehen.
+              </p>
+              <br>
+              <br>
+              <h3 style="font-size: 2.5rem;" class="title">Beispiele</h3>
+              <p>Wenn du ein Gespräch beginnst. Beispielsweise ein Supportgespräch.
+                <br>Wenn du schnell Hilfe haben möchtest, solltest du auf keinen Fall das Gespräch mit einer der folgenden Phrasen beginnen.</p><br>
+              <p class="blockquote blockquote-danger text-light">
+                „Hallo, sind Sie da?“
+                <br>„Hallo, kurze Frage.“
+                <br>„Hast du einen Augenblick?“
+                <br>„ping“ oder „bump!“
+                <br>usw
+              </p>
+              <br>
+              <br>
+              <p>Vielleicht versuchst du es mal mit einem dieser Beispiele.</p><br>
+              <p class="blockquote blockquote-success text-light">
+                „Hey, wollte kurz mitteilen, dass ich einen Fehler gefunden habe“
+                <br>„Hallo, wurde von XYZ bestohlen. Wollte ihn melden. ...“
+                <br>„Guten Tag, ich habe einen Kauf getätigt am <?php echo date("d.m.y") . " um " . date("H:i") ?> Uhr und es nicht erhalten.“
+              </p>
+              <br>
+              <p><span class="text-info">KURZ GESAGT:</span> Rücke einfach direkt mit deinem Anliegen raus!<br>Damit ist jeder Seite am schnellsten geholfen!</p>
+              <br>
+              <br>
+              <?php if ($signature == true) { ?>
+              <p><span class="text-white" style="font-size: 1rem;">Dein <?php echo $domain3 ?></span></p>
+              <?php } ?>
+            </div>
+          </div>
 
-
-<div class="h-screen pb-14 bg-right bg-cover" style="background-image:url('assets/images/bg.svg');">
-	<!--Nav-->
-	<div class="w-full container mx-auto p-6">
-			
-		<div class="w-full flex items-center justify-between">
-			<a class="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"  href="#"> 
-				 <img class="imgShadow" src="assets/images/nichthallo.png" width="45px" height="auto" /> NichtHallo.DE
-			</a>
-			
-			<?php if ($usage == true) { ?>
-				<div class="flex w-1/2 justify-end content-center">		
-					<a class="inline-block text-blue-300 no-underline hover:text-indigo-800 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4" href="/benutzen">
-						<i class="fa-solid fa-circle-info fa-2xl"></i>	
-					</a>
-				</div>
-			<?php } ?>
-			
-		</div>
-
-	</div>
-
-	<!--Main-->
-	<div class="container pt-24 md:pt-48 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-		
-		<!--Left Col-->
-		<div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
-			<h1 class="my-4 text-3xl md:text-5xl text-purple-800 font-LO font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Sage nicht einfach nur "Hallo" im Chat!</h1>
-			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Stell dir vor, du bittest um Hilfe im Support<?php echo $domain1 ?> und möchtest schnell Hilfe bekommen.</p>
-			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Genau das ist das Ziel der meisten Plattformen, Firmen, Anbieter, Dienstleister, usw., dir die schnellstmögliche Hilfe anzubieten.</p>
-			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Nun schreibst du aber nur <i>"Hallo!"</i> um ein Supportgespräch zu beginnen.</p>
-			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Das ist absolut nicht von Vorteil und erschwert das Ziel, schnelle Hilfe zu bieten, ungemein.</p>
-			<p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Probier es doch einfach mal mit einer direkten Beschreibung deines Anliegens.<br>
-			Ein Support-Mitarbeiter<?php echo $domain2 ?> wird sich so-oder-so zurückmelden, um dein Anliegen anzugehen.</p>
-		</div>
-		
-		<!--Right Col-->
-		<div class="w-full xl:w-2/6 py-6 overflow-y-hidden">
-			<div style="margin-left: 45px; margin-bottom: 30px;">
-				<h4 class="my-4 text-3xl md:text-4xl text-red-500 font-LO font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Nicht so...</h4>
-				<ul style="margin-left:15px">
-					<li class="li-ns slide-in-bottom-h1 leading-normal text-base md:text-1xl">"Hallo, sind Sie da?"</li>
-					<li class="li-ns slide-in-bottom-h1">"Hallo, kurze Frage."</li>
-					<li class="li-ns slide-in-bottom-h1">"Hast du einen Augenblick?"</li>
-					<li class="li-ns slide-in-bottom-h1">"ping" / "bump!"</li>
-					<li class="li-ns slide-in-bottom-h1">u.s.w</li>
-				</ul>
-			</div>
-			<div style="margin-left: 45px">
-				<h4 class="my-4 text-3xl md:text-4xl text-green-500 font-LO font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Sondern so!</h4>
-				<ul style="margin-left:15px">
-					<li class="li-ss slide-in-bottom-h1">"Hey, wollte kurz mitteilen, dass ich einen Fehler gefunden habe. <Fehlerbeschreibung>..."</li>
-					<li class="li-ss slide-in-bottom-h1">"Hallo, wurde von XYZ bestohlen. Wollte ihn melden. <Beweis/Screenshot>..."</li>
-					<li class="li-ss slide-in-bottom-h1">"Guten Tag, ich habe einen Kauf getätigt am <?php echo date("m.d.y") . " um " . date("H:i") ?> Uhr und es nicht erhalten."</li>
-					<li class="slide-in-bottom-h1" style="margin-top: 15px;"><b>KURZ GESAGT:</b> Rücke einfach direkt mit deinem Anliegen raus!<br>Damit ist jeder Seite am schnellsten geholfen!</li>
-				</ul>
-			</div>
-		</div>
-		
-		<?php if ($signature == true) { ?>
-			<p class="text-blue-400 font-bold pb-8 lg:pb-6 text-center md:text-left fade-in"><?php echo $domain3 ?></p>
-		<?php } ?>
-		<?php if ($footer == true) { ?>
-			<div class="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in" >
-				<a class="text-gray-500 no-underline hover:no-underline" href="mailto:kontakt@keee.vin">&copy; 2019-<?php echo date("Y") ?> DriverSpy</a>
-			</div>
-		<?php } ?>
-		
-	</div>
-	
-
-</div>
+        </div>
+      </div>
+      
+    </div>
+    <?php if ($footer == true) { ?>
+    <footer class="footer" data-background-color="black">
+      <div class="container">
+        <a class="footer-brand" href="">NichtHallo.DE</a>
+        <ul class="pull-center">
+          <li>
+            <p>
+              &copy; 2019-<?php echo date('Y') ?> NichtHalloDE
+            </p>
+          </li>
+        </ul>
+        <ul class="social-buttons pull-right">
+          <li>
+            <a href="https://github.com/NichtHalloDE" class="btn btn-icon btn-neutral btn-link">
+              <i class="fab fa-github"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+    <?php } ?>
+  </div>
 </body>
+
+<script>
+  if (document.getElementById('typed')) {
+      var typed = new Typed("#typed", {
+          typeSpeed: 90,
+          backSpeed: 90,
+          backDelay: 1200,
+          smartBackspace: true,
+          startDelay: 500,
+          loop: true,
+          showCursor: false,
+          fadeOut: true,
+          fadeOutClass: 'typed-fade-out',
+          fadeOutDelay: 700,
+          strings: [
+              'Hallo',
+              'Hi',
+              'Kurze Frage',
+          ],
+      });
+  }
+</script>
+
 </html>
